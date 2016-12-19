f0de2aea5e61e92d11ba58967c6ff4345b8d22b4
Cayla Zabel <Cayla@Caylas-MacBook-Air.local>
2016-12-19 15:50:19 -0800

    started render function

--- game.js
+++ game.js
@@ -4,14 +4,21 @@ var charImgTopLeft = document.getElementById('char-img-top-left');
 var charImgTopRight = document.getElementById('char-img-top-right');
 var charImgBtmLeft = document.getElementById('char-img-btm-left');
 var charImgBtmRight = document.getElementById('char-img-btm-right');
+var test = 3000;
 
-function renderPosition(position) {
+function renderPosition(position, timeToShow) {
+  console.log('entering render position');
   position.style.display = 'inline-block';
   window.setTimeout(function(){
     position.style.display = 'none';
-  }, 1000);
+  }, timeToShow);
 }
+// for (var i = 0; i < 2; i++){
+// renderPosition(charImgTopLeft, test * (i+1));
+// }
+for (var i = 0; i < 2; i++)
+renderPosition(charImgTopLeft, 2000);
+window.setTimeout(function(){
+  renderPosition(charImgBtmRight, 2000)
 
-renderPosition(charImgTopRight);
-renderPosition(charImgBtmRight);
-renderPosition(charImgTopLeft);
+}, 2000);
