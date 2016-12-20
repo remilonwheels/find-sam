838e437c96008a0da92dd1869482afffdf9f0f26
Adrian <JS02137@gmail.com>
2016-12-20 15:55:49 -0800

    Completed functionality for leaderboard page

--- leaderpage.js
+++ leaderpage.js
@@ -5,8 +5,11 @@ function Score (userName, score) {
   this.userName = userName;
   this.score = score ;
   scoreArray.push(this);
+
 }
+
 // The following were instances used for testing and nothing more.
+
 new Score('Sideshow Bob', 2);
 new Score('Krusty the Clown', 6);
 new Score('Montgomery Burns', 3);
@@ -14,14 +17,28 @@ new Score('Mr. Smithers', 8);
 new Score('Groundskeeper Willie', 4);
 
 scoreArray.sort(function(a, b) {
-  return parseFloat(b.score) - parseFloat(a.score);
-
+return parseFloat(b.score) - parseFloat(a.score);
 });
-function renderList() {
+var playerName = [];
+var playerScore = [];
+var mats = [];
+
+function makeList() {
   for (var i = 0; i < scoreArray.length; i++) {
-  var listItem = document.createElement('li');
-  leaderList.appendChild('listItem');
+    playerName[i] = scoreArray[i].userName;
+    playerScore[i] = scoreArray[i].score;
+    mats.push("<li>" + playerName[i] + playerScore[i] +"</li>");
 
+  }
 
 }
+function renderList() {
+  for(var i = 0; i < scoreArray.length; i++) {
+    console.log(playerName[i] + playerScore[i]);
+    leaderList.innerHTML = mats;
+
+  }
+
 }
+makeList();
+renderList();
