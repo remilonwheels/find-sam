d42f6805446cd63fb475c6cfeb416dd0eb41dd3e
Adrian <JS02137@gmail.com>
2016-12-20 11:43:40 -0800

    Started working on function to sort array for leaderboard

--- leaderboard.js
+++ leaderboard.js
@@ -1,13 +1,17 @@
 'use strict';
-var scoreArray = [];
+var newArray = [scoreArray];
 function Score (userName) {
   this.userName = userName;
   this.score = 0;
   scoreArray.push(this);
 }
+
 new Score('James', 2);
 new Score('Robert', 6);
 new Score('Yaroslav', 3);
 new Score('Пошелнахуи', 8);
 new Score('James K. Polk', 4);
-Math.max(scoreArray);
+
+scoreArray.sort (function(a,b) {
+  return (b-a)
+}
