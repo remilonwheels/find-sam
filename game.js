201af9a02c4c427d70dae1878c16e24d86ba405f
Cayla Zabel <Cayla@Caylas-MacBook-Air.local>
2016-12-19 16:52:01 -0800

    functioningish render function

--- game.js
+++ game.js
@@ -4,21 +4,24 @@ var charImgTopLeft = document.getElementById('char-img-top-left');
 var charImgTopRight = document.getElementById('char-img-top-right');
 var charImgBtmLeft = document.getElementById('char-img-btm-left');
 var charImgBtmRight = document.getElementById('char-img-btm-right');
-var test = 3000;
+var showHideTime = 1000;
 
-function renderPosition(position, timeToShow) {
+function renderPosition(position, timeToShow, timeToHide) {
   console.log('entering render position');
-  position.style.display = 'inline-block';
+  window.setTimeout(function(){
+    position.style.display = 'inline-block';
+  }, timeToHide);
   window.setTimeout(function(){
     position.style.display = 'none';
   }, timeToShow);
 }
-// for (var i = 0; i < 2; i++){
-// renderPosition(charImgTopLeft, test * (i+1));
-// }
-for (var i = 0; i < 2; i++)
-renderPosition(charImgTopLeft, 2000);
-window.setTimeout(function(){
-  renderPosition(charImgBtmRight, 2000)
 
-}, 2000);
+var positionArray = [charImgTopLeft, charImgTopRight, charImgBtmLeft, charImgTopLeft, charImgBtmRight, charImgBtmRight]
+
+for (var i = 0; i < positionArray.length; i++){
+  // renderPosition(positionArray[i], test * (i+1), test * (i));
+  // window.setTimeout(function(){
+  //   renderPosition(positionArray[i], test * (i+1), test * (i));
+  // }, test * (i+1));
+  renderPosition(positionArray[i], showHideTime * (i + 1), showHideTime * i);
+}
