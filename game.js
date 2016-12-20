15c8a3efb5e79d56f28dec93cbd3607be6bdbfb2
Remil Marzan <remilonwheels@gmail.com>
2016-12-20 09:48:21 -0800

    click event handler tracks user clicks and compares to position

--- game.js
+++ game.js
@@ -4,10 +4,10 @@ var charImgTopLeft = document.getElementById('char-img-top-left');
 var charImgTopRight = document.getElementById('char-img-top-right');
 var charImgBtmLeft = document.getElementById('char-img-btm-left');
 var charImgBtmRight = document.getElementById('char-img-btm-right');
-var showHideTime = 1000;
+
+var positionArray = [charImgTopLeft, charImgTopRight, charImgBtmLeft, charImgTopLeft, charImgBtmRight, charImgBtmRight];
 
 function renderPosition(position, timeToShow, timeToHide) {
-  console.log('entering render position');
   window.setTimeout(function(){
     position.style.display = 'inline-block';
   }, timeToHide);
@@ -16,25 +16,51 @@ function renderPosition(position, timeToShow, timeToHide) {
   }, timeToShow);
 }
 
-var positionArray = [charImgTopLeft, charImgTopRight, charImgBtmLeft, charImgTopLeft, charImgBtmRight, charImgBtmRight]
-
-for (var i = 0; i < positionArray.length; i++){
-  // renderPosition(positionArray[i], test * (i+1), test * (i));
-  // window.setTimeout(function(){
-  //   renderPosition(positionArray[i], test * (i+1), test * (i));
-  // }, test * (i+1));
-  renderPosition(positionArray[i], showHideTime * (i + 1), showHideTime * i);
+function renderPositionArray(array, showTime, hideTime){
+  for (var i = 0; i < array.length; i++){
+    renderPosition(array[i], (showTime * (i + 1)) + (hideTime * i), (showTime + hideTime) * i);
+  }
 }
+
+renderPositionArray(positionArray, 500, 250);
 
 
 var gameboard = document.getElementById('gameboard');
+var clickCount = 0;
 
 function handleUserClick(event) {
   event.preventDefault();
 
-  var userSelection = event.target.id;
+  var userSelection;
 
-  console.log(userSelection);
+  if (event.target.id === 'img-top-left') {
+    userSelection = 'char-img-top-left';
+  }
+  if (event.target.id === 'img-top-right') {
+    userSelection = 'char-img-top-right';
+  }
+  if (event.target.id === 'img-btm-left') {
+    userSelection = 'char-img-btm-left';
+  }
+  if (event.target.id === 'img-btm-right') {
+    userSelection = 'char-img-btm-right';
+  }
+
+  if (userSelection === positionArray[clickCount].id) {
+
+  } else {
+    console.log('user lose');
+    //write userLose() function
+    clickCount = 0;
+  }
+
+  if (clickCount === positionArray.length - 1) {
+    return console.log('user win');
+    //write userWin() function
+    return userWin();
+  }
+  clickCount += 1;
+  console.log(clickCount);
 }
 
 gameboard.addEventListener('click', handleUserClick);
