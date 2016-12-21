8b52e2a37710859c022515258d10e7a36d654e40
Khalid Mohamud <kmohamud1@outlook.com>
2016-12-21 10:48:11 -0800

    its not workinh

--- index.js
+++ index.js
@@ -2,13 +2,25 @@
 var userSignIn = document.getElementById('UserSignIn');
 userSignIn.addEventListener('submit', SignIn);
 var scoreArray = [];
+var button = document.getElementById('button');
+
+
+// function takeToGame() {
+//   location.href= 'game.html';
+// }
 
 function SignIn(event) {
   event.preventDefault();
   var userName = event.target.UserName.value;
-  console.log(userName);
-  new Score(userName);
-  localStorage.setItem('Score', JSON.stringify(scoreArray));
+  if(userName.length < 1) {
+    alert('Sorry, write a name!!!');
+    button.location.href= 'index.html';
+  } else {
+    console.log(userName);
+    new Score(userName);
+    localStorage.setItem('Score', JSON.stringify(scoreArray));
+    button.onclick.innerHTML= 'game.html';
+  }
 }
 
 function Score (userName) {
